6ae88c529f42a553983b2589756919aaed8e52c9
devjasin <developer.jasin@gmail.com>
2024-10-06 20:08:15 +0545

    valdation for reset password

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import { promisify } from "util";
 import db from "../model/index.js";
 import decodeToken from "../services/decodeToken.js";
+import bcrypt from "bcryptjs/dist/bcrypt.js";
+import sendEmail from "../services/sendEmail.js";
 
 const isAuthenticated = async (req, res, next) => {
   const token = req.cookies.token;
@@ -22,19 +24,133 @@ const isAuthenticated = async (req, res, next) => {
     next();
   }
 };
-
-const handlePassWordChange = (req, res) => {
+const forgotPasswordForm = async (req, res) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      res.status(400).json({ message: "bad request" });
+    }
+    const emailExists = await db.users.findAll({
+      where: {
+        email: email,
+      },
+    });
+    if (emailExists.length === 0) {
+      res.send("user with that email doesnot exist");
+    } else {
+      //send opt on that email
+      const generateOtp = Math.floor(1000 + Math.random() * 9000);
+      await sendEmail({
+        email: email,
+        subject: "reset password",
+        otp: generateOtp,
+      });
+      emailExists[0].otp = generateOtp;
+      emailExists[0].otpgenerateTime = Date.now();
+      await emailExists[0].save();
+      res.redirect("/forgotpassword/otp?email=" + email);
+    }
+  } catch (error) {
+    console.log("error during fetching data" + error);
+  }
+};
+const ForgotpasswordGet = (req, res) => {
+  res.render("forgotpassword");
+};
+const verifyOtpGet = (req, res) => {
+  const email = req.query.email;
+  res.render("verifyOTP", { email });
+};
+const verifyOtpPost = async (req, res) => {
+  const { otp } = req.body;
+  const email = req.params.id;
+  if (!otp || !email) {
+    res.status(400).json({ message: " bad request" });
+  }
+  const doesExist = await db.users.findAll({
+    where: {
+      otp: otp,
+      email: email,
+    },
+  });
+  if (doesExist.length > 0) {
+    const currentUserTime = Date.now();
+    const otpgenerateTime = doesExist[0].otpgenerateTime;
+    if (currentUserTime - otpgenerateTime <= 120000) {
+      /*   emailExists[0].otp = null;
+      emailExists[0].otpgenerateTime = null;
+      await emailExists[0].save(); */
+      // res.cookie("teset".email)
+      // res.redirect("/forgotpassword/passwordchange?email=" + email);
+      res.redirect(`/forgotpassword/passwordchange?email=${email}&otp=${otp}`);
+    } else {
+      res.send("otp has expired");
+    }
+    //1second=12000 milisecond
+  } else {
+    res.status(400).json({ message: " Invalid otp" });
+  }
+};
+const forGetPasswordGet = (req, res) => {
+  const email = req.query.email;
+  const otp = req.query.otp;
+  if (!email || !otp) {
+    res.redirect("/login");
+    return;
+  }
+  res.render("passwordchange", { email, otp });
+};
+const handlePassWordChange = async (req, res) => {
+  // req.cookies.teste
   const { newPassword, confirmPassword } = req.body;
-  if (!newPassword || !confirmPassword) {
+  const email = req.query.email;
+  const otp = req.query.otp;
+  if (!newPassword || !confirmPassword || !email) {
     return res.status(400).send("password must be provided");
   }
   if (newPassword !== confirmPassword) {
-    res.send("password doesnot match");
+    res.send("new password and confirm password must be match");
   } else {
+    /*    const userData= db.users.findAll({ where: { email: email } });
+    userData[0].password=newPassword
+    await userData[0].save()
+ */
+    try {
+      const dotp = await db.users.findAll({
+        where: { email: email, otp: otp },
+      });
+      if (dotp[0].otp !== otp) {
+        res.send("invalid otp");
+        return;
+      } else {
+        const currentUserTime = Date.now();
+        const otpgenerateTime = dotp[0].otpgenerateTime;
+        if (currentUserTime - otpgenerateTime <= 240000) {
+          await db.users.update(
+            { password: bcrypt.hashSync(newPassword) },
+            { where: { email: email } }
+          );
+          res.redirect("/login");
+        } else {
+          res.send("otp has expired");
+        }
+      }
+    } catch (error) {
+      res.send("invalid input");
+      console.log("invalid input please try agian");
+    }
   }
 };
 
-export { isAuthenticated, handlePassWordChange };
+export {
+  isAuthenticated,
+  handlePassWordChange,
+  forgotPasswordForm,
+  ForgotpasswordGet,
+  verifyOtpGet,
+  verifyOtpPost,
+  forGetPasswordGet,
+};
 
 /* here promisify handle (error,decode section),with out this it look like 
 
--- router/forgotpassword.js
+++ router/forgotpassword.js
@@ -1,84 +1,21 @@
 import express, { Router } from "express";
-import db from "../model/index.js";
-import sendEmail from "../services/sendEmail.js";
+import {
+  forGetPasswordGet,
+  forgotPasswordForm,
+  ForgotpasswordGet,
+  handlePassWordChange,
+  verifyOtpGet,
+  verifyOtpPost,
+} from "../middleware/isAuthenticated.js";
 
 const Forgotpassword = Router();
 
-Forgotpassword.route("/")
-  .get((req, res) => {
-    res.render("forgotpassword");
-  })
-  .post(async (req, res) => {
-    try {
-      const { email } = req.body;
-      if (!email) {
-        res.status(400).json({ message: "bad request" });
-      }
-      const emailExists = await db.users.findAll({
-        where: {
-          email: email,
-        },
-      });
-      if (emailExists.length === 0) {
-        res.send("user with that email doesnot exist");
-      } else {
-        //send opt on that email
-        const generateOtp = Math.floor(1000 + Math.random() * 9000);
-        await sendEmail({
-          email: email,
-          subject: "reset password",
-          otp: generateOtp,
-        });
-        emailExists[0].otp = generateOtp;
-        emailExists[0].otpgenerateTime = Date.now();
-        await emailExists[0].save();
-        res.redirect("/forgotpassword/otp?email=" + email);
-      }
-    } catch (error) {
-      console.log("error during");
-    }
-  });
+Forgotpassword.route("/").get(ForgotpasswordGet).post(forgotPasswordForm);
 
-Forgotpassword.route("/otp").get((req, res) => {
-  const email = req.query.email;
-  res.render("verifyOTP", { email });
-});
-Forgotpassword.route("/otp/:id").post(async (req, res) => {
-  const { otp } = req.body;
-  const email = req.params.id;
-  if (!otp || !email) {
-    res.status(400).json({ message: " bad request" });
-  }
-  const doesExist = await db.users.findAll({
-    where: {
-      otp: otp,
-      email: email,
-    },
-  });
-  console.log(doesExist);
-  if (doesExist.length > 0) {
-    const currentUserTime = Date.now();
-    const otpgenerateTime = doesExist[0].otpgenerateTime;
-    if (currentUserTime - otpgenerateTime <= 120000) {
-      /*   emailExists[0].otp = null;
-      emailExists[0].otpgenerateTime = null;
-      await emailExists[0].save(); */
-      res.redirect("/forgotpassword/passwordchange");
-    } else {
-      res.send("otp has expired");
-    }
-    //1second=12000 milisecond
-  } else {
-    res.status(400).json({ message: " Invalid otp" });
-  }
-});
-
+Forgotpassword.route("/otp").get(verifyOtpGet);
+Forgotpassword.route("/otp/:id").post(verifyOtpPost);
 Forgotpassword.route("/passwordchange")
-  .get((req, res) => {
-    res.render("passwordchange");
-  })
-  .post((req, res) => {
-    const { password } = req.body;
-  });
+  .get(forGetPasswordGet)
+  .post(handlePassWordChange);
 
 export { Forgotpassword };
